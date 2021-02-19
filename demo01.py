f9cf32b2daa921f16330b8891666e87bdc07dfd2
zhuang-z <1372427659@qq.com>
2021-02-19 17:29:35 +0800

    第二次

--- demo01.py
+++ demo01.py
@@ -45,8 +45,18 @@ print("input获取的内容：",a+b)  #input获取的内容都是以字符串的
 # print(type(len(a)))
 
 #计算两个字符串长度之和
-a = input("请输入a:")
-b = input("请输入b:")
-c = len(a)
-d = len(b)
-print("a和b的长度之和为：",c+d)
+# a = input("请输入a:")
+# b = input("请输入b:")
+# c = len(a)
+# d = len(b)
+# print("a和b的长度之和为：",c+d)
+
+#元组  下标 0开始
+
+a = (1,2,5,"哈哈","dsa","xixi","xixi",True,False)
+# print(a[6])  #输出a元组第七个元素
+
+print(a.index("xixi"))  #输出xixi的下标，从左至右就近原则
+print(a.count("xixi"))  #打印有多少个"xixi"
+
+
